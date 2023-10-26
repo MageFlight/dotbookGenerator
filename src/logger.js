dabda8dc5c1518ca2bf2bed396a17b9bfbe486d3
MageFlight <112671607+MageFlight@users.noreply.github.com>
2023-10-26 10:57:19 +0000

    Add css styling

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,11 @@
 const errorLog = document.querySelector("#errorText");
 
 export function logError(message) {
+    errorLog.style.display = "block";
     errorLog.textContent = message;
 }
 
 export function resetLog() {
+    errorLog.style.display = "none";
     errorLog.textContent = "";
 }
